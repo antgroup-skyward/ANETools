9f742bff3e9965e499c824179800834840d6827b
Wish Wu <wish199202@hotmail.com>
2021-02-22 11:29:13 +0800

    replace to real file

--- coremlc.sh
+++ coremlc.sh
@@ -2,4 +2,5 @@ if [ -z $1 ] || [ -z $2 ] ; then
     echo "usage $0 <input .mlmodel file> <output dir for .mlmodelc>"
     exit -1
 fi
-/Applications/Xcode.app/Contents/Developer/usr/bin/coremlc compile $1 $2 --deployment-target 13.0 --platform ios
+#/Applications/Xcode.app/Contents/Developer/usr/bin/coremlc compile $1 $2 --deployment-target 13.0 --platform ios
+/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/coremlcompiler compile $1 $2 --deployment-target 13.0 --platform ios
